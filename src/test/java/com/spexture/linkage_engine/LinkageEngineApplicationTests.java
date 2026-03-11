b84fc99475d139343653cea307d41d46372f0460
sbecker11 <sbecker11@me.com>
2026-03-10 18:31:21 -0600

    initial commit

---
+++ src/test/java/com/spexture/linkage_engine/LinkageEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spexture.linkage_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkageEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
